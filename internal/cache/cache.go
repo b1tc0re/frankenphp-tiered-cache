4ff32ea1dd772f76f83098d7e6c5e5344f157e79
Дмитрий <b1tc0re@yandex.ru>
2026-09-11 23:56:44 +0300

    Refine cache interface semantics

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -6,15 +6,15 @@ import (
 )
 
 var (
-	ErrInvalidTTL   = errors.New("cache: ttl must be greater than zero")
-	ErrItemTooLarge = errors.New("cache: item exceeds maximum size")
-	ErrCacheFull    = errors.New("cache: unable to free enough memory")
+	ErrInvalidTTL = errors.New("cache: ttl must be greater than zero")
+	ErrNilValue   = errors.New("cache: nil value is not allowed")
 )
 
 type Cache interface {
-	Get(key string) ([]byte, bool, error)
-	Set(key string, value []byte, ttl time.Duration) error
-	Forever(key string, value []byte) error
-	Forget(key string) error
-	Touch(key string, ttl time.Duration) error
+	Get(key string) ([]byte, error)
+	Set(key string, value []byte, ttl time.Duration) (bool, error)
+	Forever(key string, value []byte) (bool, error)
+	Forget(key string) (bool, error)
+	Touch(key string, ttl time.Duration) (bool, error)
+	Flush() (bool, error)
 }
