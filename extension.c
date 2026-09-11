02c7be4ab94159d63901c9270ca1cae94ea58d17
Дмитрий <b1tc0re@yandex.ru>
2026-09-11 19:48:19 +0300

    Add PHP module entry

---
+++ extension.c
@@ -0,0 +1,14 @@
+#include "extension.h"
+
+zend_module_entry franken_tiered_module_entry = {
+    STANDARD_MODULE_HEADER,
+    "franken_tiered",
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    "0.0.0-dev",
+    STANDARD_MODULE_PROPERTIES
+};
